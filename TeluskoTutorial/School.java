ee8fd59de989be8350079dc9a48e5530963e2cc5
Manasseh Ankrah <solomonankrah24@gmail.com>
2024-01-03 12:59:00 +0000

    School Class

---
+++ TeluskoTutorial/School.java
@@ -0,0 +1,12 @@
+package TeluskoTutorial;
+
+public class School {
+    int num;
+    double average;
+    boolean isFavourite;
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+
+}
