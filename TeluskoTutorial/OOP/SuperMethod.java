5433bbf25c56baaa969e75531a6c49ebabaceb03
Manasseh Ankrah <solomonankrah24@gmail.com>
2024-01-12 18:21:55 +0000

    Practiced SuperMethod in Inheritance

---
+++ TeluskoTutorial/OOP/SuperMethod.java
@@ -0,0 +1,31 @@
+package TeluskoTutorial.OOP;
+
+
+class A {
+    public A() {
+        System.out.println("Initial A");
+    }
+
+    public A(int a) {
+        System.out.println("Initial A value = " + a);
+    }
+}
+
+class B extends A {
+    public B() {
+        System.out.println("Initial B");
+    }
+
+    public B(int b) {
+        super(b);
+        System.out.println("Initial B value = " + b);
+    }
+}
+
+public class SuperMethod {
+    public static void main(String[] args) {
+        B secondVal = new B(5);
+
+
+    }
+}
