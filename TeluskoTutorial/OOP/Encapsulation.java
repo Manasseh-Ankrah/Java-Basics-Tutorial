cb1f37ee5cbe25814fccd2a650393c5479d26fed
Manasseh Ankrah <solomonankrah24@gmail.com>
2024-01-29 12:31:37 +0000

    Practicing Encapsulation

---
+++ TeluskoTutorial/OOP/Encapsulation.java
@@ -0,0 +1,42 @@
+package TeluskoTutorial.OOP;
+
+
+class Operations {
+    int num_1;
+    int num_2;
+
+    // Setters
+    public void setNum_1(int num_1) {
+        this.num_1 = num_1;
+    }
+
+    public void setNum_2(int num_2) {
+        this.num_2 = num_2;
+    }
+
+    // Getters
+    public int getNum_1() {
+        return num_1;
+    }
+
+    public int getNum_2() {
+        return num_2;
+    }
+}
+public class Encapsulation {
+    public static void main (String[] Args){
+        Operations opp = new Operations();
+        opp.num_1 = 3;
+
+        opp.setNum_1(3);
+        opp.setNum_2(50);
+
+        int first= opp.getNum_1();
+        int second = opp.getNum_2();
+
+
+        System.out.println("First Number => " + first);
+        System.out.println("Second Number => " + second);
+
+    }
+}
--- TeluskoTutorial/OOP/SuperMethod.java
+++ TeluskoTutorial/OOP/SuperMethod.java
@@ -25,7 +25,5 @@ class B extends A {
 public class SuperMethod {
     public static void main(String[] args) {
         B secondVal = new B(5);
-
-
     }
 }
